8226e1d2ad5d9ae917a6ebe123a3a510ded3538c
Quentin Jin <jianstm@gmail.com>
2019-06-02 11:09:16 +0800

    Add `and` method

--- Sources/FutureQ/Features/and.swift
+++ Sources/FutureQ/Features/and.swift
@@ -9,7 +9,11 @@ import Foundation
 
 extension Thenable {
     
-//    public func and<U: Thenable>(_ thenable: U) -> Future<(T, U)> {
-//        
-//    }
+    public func and<U: Thenable>(_ thenable: U) -> Future<(T, U.T)> {
+        return Future<(T, U.T)>.whenAllSucceed(self, thenable)
+    }
+    
+    public func and<U>(_ u: U) -> Future<(T, U)> {
+        return self.mapValue { ($0, u) }
+    }
 }
