ce6949d72eee604940d04f2fd54154d2a5686de5
Luo Xiu <luoxiustm@gmail.com>
2019-09-11 10:11:57 +0800

    add pub.asFuture

--- Sources/Future/Publisher.swift
+++ Sources/Future/Publisher.swift
@@ -1,11 +1,37 @@
 #if canImport(Combine)
 import Combine
 
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension Future {
     
-    @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-    public var publisher: Combine.Future<Success, Failure> {
+    public func asPublisher() -> Combine.Future<Success, Failure> {
         return .init(self.whenComplete)
+    }
+}
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension Publisher {
+    
+    public func asFuture(whenNoOutput: @autoclosure @escaping () -> Failure) -> Future<Output, Failure> {
+        let p = Promise<Output, Failure>()
+        
+        var cancel: Cancellable? = self.sink(receiveCompletion: { (c) in
+            switch c {
+            case .failure(let e):   p.fail(e)
+            case .finished:         p.fail(whenNoOutput())
+            }
+        }, receiveValue: { v in
+            p.succeed(v)
+        })
+        
+        let future = p.future
+        
+        future.whenComplete { _ in
+            cancel?.cancel()
+            cancel = nil
+        }
+        
+        return future
     }
 }
 
